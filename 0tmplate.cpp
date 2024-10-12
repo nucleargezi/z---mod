33ccf47592143c14e34324795df081355a1be8d1
guiding star <604223110@qq.com>
2024-10-12 23:06:54 +0800

    master

--- 0tmplate.cpp
+++ 0tmplate.cpp
@@ -347,8 +347,9 @@ namespace MeIoN_Pre_Things {
 void pre_work() {
     //          /*  MeIoN_is_UMP45  */          codeforces id
     //              /*  MeIoN  */               luogu / atcoder id
-    // https://space.bilibili.com/285769347     My bilibili
-    // https://www.cnblogs.com/guidingstar      My blog
+    //  https://space.bilibili.com/285769347    My bilibili
+    //   https://www.cnblogs.com/guidingstar    My blog
+    //    https://github.com/nucleargezi        My github
     //             /* 604223110 */              QQ
     
     std::cout << std::fixed << std::setprecision(12);
